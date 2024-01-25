9f5a2e7c5eedc06f07f11f1383adc054e7f05341
vinnie <githinjigicheru146@gmail.com>
2024-01-25 20:38:47 +0300

    Tests passed

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-// Write your solution in this file!
+const employee = {
+    name: "Vincent",
+    streetAddress: "Sandy Park Esate"
+  };
+  
+  function updateEmployeeWithKeyAndValue(driver, key, value) {
+    return Object.assign({}, driver, { [key]: value });
+  }
+  
+  function destructivelyUpdateEmployeeWithKeyAndValue(driver, key, value) {
+    driver[key] = value;
+  
+    return driver;
+  }
+  
+  function deleteFromEmployeeByKey(driver, key) {
+    const newObj = Object.assign({}, driver);
+  
+    delete newObj[key];
+  
+    return newObj;
+  }
+  
+  function destructivelyDeleteFromEmployeeByKey(driver, key) {
+    delete driver[key];
+  
+    return driver;
+  }
